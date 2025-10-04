5363c6e9ec18a231419ef3e651a4dacfd867c079
poheniks <66043010+poheniks@users.noreply.github.com>
2025-10-03 19:44:58 -0700

    Add project files.

---
+++ NoSpeedLimitComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Unity.IL2CPP;
+using UnityEngine;
+
+
+namespace NoSpeedLimit
+{
+    public class NoSpeedLimitComponent : MonoBehaviour
+    {
+        void Start()
+        {
+            NoSpeedLimitPlugin.BaseGroundSpeedLimit = Conf.s.maxGroundSpeed;
+            NoSpeedLimitPlugin.BaseAirSpeedLimit = Conf.s.maxAirSpeed;
+
+            Conf.s.maxGroundSpeed = float.MaxValue;
+            Conf.s.maxAirSpeed = float.MaxValue;
+        }
+    }
+}
---
+++ NoSpeedLimitPlugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+
+namespace NoSpeedLimit
+{
+    [BepInPlugin("poh.bepinex.plugins.nospeedlimit", "No Speed Limit", "1.0.0")]
+    public class NoSpeedLimitPlugin : BasePlugin
+    {
+        public static NoSpeedLimitPlugin? Instance { get; private set; }
+
+        public static float BaseGroundSpeedLimit;
+        public static float BaseAirSpeedLimit;
+
+        public override void Load()
+        {
+            Log.LogInfo($"Plugin No Speed Limit is loaded!");
+            AddComponent<NoSpeedLimitComponent>();
+
+            Instance = this;
+        }
+
+        public override bool Unload()
+        {
+            Conf.s.maxGroundSpeed = BaseGroundSpeedLimit;
+            Conf.s.maxAirSpeed = BaseAirSpeedLimit;
+            Log.LogInfo($"Plugin No Speed Limit unloaded. Speed limits set to default values");
+
+            Instance = null;
+            return true;
+        }
+    }
+}
